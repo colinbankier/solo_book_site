1fb0084f6f31aaa399c3c60bea4b0de434bfc099
colinbankier <colinbankier@gmail.com>
2014-10-23 07:24:06 +1000

    image process.

--- image_process.sh
+++ image_process.sh
@@ -1,14 +1,16 @@
 
 original_path="/Users/colin.bankier/Documents/Solo Webstuff/originals"
-for image in under_sail.jpg bay.jpg beached.tif tied_up.tif sunset.jpg book.tif contents.jpg ice.tif mary_vic_allison.tif morea.tif vic.jpg;
+#for image in under_sail.jpg bay.jpg beached.tif tied_up.tif sunset.jpg book.tif contents.jpg ice.tif mary_vic_allison.tif morea.tif vic.jpg;
+for image in contents.jpg;
 do
   voffset=0
   ext=jpg
-  if [ $image == "under_sail" ]; then
+  filename="${image%.*}"
+  if [ $filename = "under_sail" ]; then
     voffset=120
   fi
-  filename="${image%.*}"
   echo $filename
-  convert -define jpeg:size=200x200 "$original_path/${image}" -gravity center -extent 200x200+0+$voffset\
-  -quality 60 -thumbnail '200x200^' assets/images/${filename}_200x200.jpg
+  echo "voffset $voffset"
+  convert -define jpeg:size=200x200 "$original_path/${image}" -gravity center -extent 200x200\
+  -quality 60 -thumbnail '200x200' assets/images/${filename}_200x200.jpg
 done
