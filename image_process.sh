fed832748214d6f7d72bd6ba23a5814c195c6442
colinbankier <colinbankier@gmail.com>
2014-11-24 07:54:53 +1000

    Image updates

--- image_process.sh
+++ image_process.sh
@@ -2,7 +2,7 @@
 
 original_path="/Users/colin.bankier/Documents/Solo Webstuff/originals"
 
-for image in under_sail.jpg bay.jpg beached.tif tied_up.tif sunset.jpg book_2.jpg contents.jpg ice.tif mary_vic_allison.tif morea.tif vic.jpg;
+for image in under_sail.jpg bay.jpg beached.tif tied_up.tif sunset.jpg book_2.jpg contents.jpg ice.tif mary_vic_allison.tif morea.tif vic.jpg page_23.jpg page_54.jpg page_84.jpg page_121.jpg page_148.jpg page_171.jpg;
 do
   voffset=0
   ext=jpg
@@ -18,9 +18,7 @@ do
   echo $filename
   echo "voffset $voffset"
   convert -define jpeg:size=200x200 "$original_path/${image}" -strip -thumbnail '200x200^' \
-    -gravity center $crop -quality 60 assets/images/${filename}_200x200.jpg
-
-  #convert -define jpeg:size=200x200 "$original_path/book_2.jpg" -thumbnail '180x180^' -gravity center  -crop 180x180+0+0\! -background white -flatten assets/images/book_200x200.jpg
+    -gravity center $crop -quality 80 assets/images/${filename}_200x200.jpg
 
   convert "$original_path/${image}" -resize 1200 -strip -format jpeg\
   -quality 60 assets/images/${filename}_fullsize.jpg
@@ -32,4 +30,4 @@ convert "$original_path/background.png" -resize 1200 -strip -format jpeg\
 
 echo cover
 convert "$original_path/cover.png" -resize 940  -strip -format jpeg\
-  -quality 60 assets/images/cover.jpg
+  -quality 90 assets/images/cover.jpg
